eaf364044b7ad6369f0e5fb92cc03a8bda4798c6
d-beige <daviyontae.floyd@testpro.io>
2025-04-11 16:40:36 -0500

    Decided to troubleshoot the test that failed 2 years ago. Everything passes now!

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,23 +1,18 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class Homework16 extends BaseTest {
     @Test
     public void registrationNavigation(){
 
-        String url2 = "https://qa.koel.app/registration.php";
+        String url2 = "https://qa.koel.app/registration";
 //      Open the url in Chrome
         openLoginLink();
 
 //        Click the Registration link
-        WebElement registrationLink = driver.findElement(By.cssSelector("#hel"));
+        WebElement registrationLink = driver.findElement(By.cssSelector("a[href='registration']"));
         registrationLink.click();
 
 //        Verify user redirected to Registration page
