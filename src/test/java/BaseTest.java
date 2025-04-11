55199108376d0b56eea89c5977395b290c055094
d-beige <daviyontae.floyd@testpro.io>
2025-04-11 17:05:38 -0500

    Decided to troubleshoot the test that failed 2 years ago. Everything passes now!

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
